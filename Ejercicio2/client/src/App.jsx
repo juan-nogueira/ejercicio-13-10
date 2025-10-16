ea49ed6d5d709846f9bd06fc2c7aa4336422067d
Amir Ale <Amir___1@outlook.com>
2025-10-16 08:39:58 -0300

    Pagina funcionando

---
+++ Ejercicio2/client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import PersonalInfo from './components/register/PersonalInfo';
+import ContactInfo from './components/register/ContactInfo';
+import Confirmation from './components/register/Confirmation';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+       <nav style={{ padding: '20px', borderBottom: '1px solid #ccc' }}>
+  <Link to="/" style={{ marginRight: '15px' }}>Inicio</Link>
+  {/* This link should point to the first step */}
+  <Link to="/register/personal">Registrarse</Link>
+</nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />}>
+            <Route path="personal" element={<PersonalInfo />} />
+            <Route path="contact" element={<ContactInfo />} />
+            <Route path="confirmation" element={<Confirmation />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
